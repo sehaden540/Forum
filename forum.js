68f4c81392832653ff5254876b2f52dde754a0bc
sehaden540 <sgalloway2010@gmail.com>
2020-12-26 13:32:14 -0800

    Added card boxes around each individual post and styled them. Also redirected to the main forum page upon post submits.

--- forum.js
+++ forum.js
@@ -15,7 +15,7 @@ function submitForm(event) {
     posts["allPosts"].push(post)
     localStorage.setItem("posts", JSON.stringify(posts))
     //console.log(post);
-    return false;
+    window.location.href = "file:///C:/Users/sgall/Documents/Web%20Development/Forum/index.html" ;
 }
 
 //function that allows for the posts submitted to be loaded on the main Forum page
@@ -23,17 +23,26 @@ function loadPosts() {
     removeAllChildNodes(document.getElementById("posts")); //this removes all elements so we do not see duplicate posts on the screen
     var tree = document.createDocumentFragment();
     const post = JSON.parse(localStorage.getItem("posts"))
-    for (let i = 0; i < post.allPosts.length; i++) {
+    for (let i = post.allPosts.length-1; i >= 0; i--) {
+        var cardDiv = document.createElement("div");
+        cardDiv.classList.add("card");
+        var cardBody = document.createElement("div");
+        cardBody.classList.add("card-body");
+
         var textMessage = document.createElement("p");
+        textMessage.classList.add("card-text");
         //textMessage.setAttribute("id", "id1");
         textMessage.appendChild(document.createTextNode(post.allPosts[i].message));
 
         var textSubject = document.createElement("p");
+        textSubject.classList.add("card-title");
         //textSubject.setAttribute("id", "id2")
         textSubject.appendChild(document.createTextNode(post.allPosts[i].subject));
 
-        tree.appendChild(textSubject);
-        tree.appendChild(textMessage);
+        cardBody.appendChild(textSubject);
+        cardBody.appendChild(textMessage);
+        cardDiv.appendChild(cardBody);
+        tree.appendChild(cardDiv);
     }
     // var text2 = document.createElement("a");
     // text2.setAttribute("href", "https://www.google.com");
@@ -43,7 +52,7 @@ function loadPosts() {
 }
 
 //function that removes all elements
-function removeAllChildNodes(parent) { 
+function removeAllChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
